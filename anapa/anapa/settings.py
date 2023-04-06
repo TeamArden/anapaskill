efdcd199486851081a18f7e27eaa895fa0b612fc
kali <teamarden7@gmail.com>
2023-04-06 20:08:25 +0300

    Готово на продакашен

--- anapa/anapa/settings.py
+++ anapa/anapa/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 
 
 
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- anapa/skill/views.py
+++ anapa/skill/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+TOKEN = (os.getenv('TOKEN'))
+
+# def index(request):
+#     return render(request, 'skill/index.html')
+
 
 def index(request):
-    return render(request, 'skill/index.html')
-
-
-
+    context = {'TOKEN': TOKEN}
+    return render(request, 'skill/index.html', context)
